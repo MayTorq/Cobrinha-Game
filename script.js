22d98f4efeb3d3b49863f73857d1a4fa708c3567
MayTorq <maytorq014@gmail.com>
2026-01-13 19:25:47 -0300

    Criada interações de movimentação
    No JS foi desenvolvido as interações básicas do Cobrinha's Game

--- script.js
+++ script.js
@@ -7,8 +7,8 @@ let intervaloJogo;
 
 function iniciar() {
   criarTabuleiro();
-  //document.addEventListener("keydown", mudarDirecao);
-  //intervaloJogo = setInterval(moverCobra, velocidade);
+  document.addEventListener("keydown", mudarDirecao);
+  intervalo = setInterval(moverCobra, velocidade);
 }
 
 function criarTabuleiro() {
@@ -47,6 +47,68 @@ function posicaoOcupada(posicao) {
   return cobra.some(
     (segmento) => segmento.x === posicao.x && segmento.y === posicao.y
   );
+}
+
+function mudarDirecao(event) {
+  const tecla = event.key;
+
+  if (tecla === "ArrowUp" && direcao !== "BAIXO") {
+    direcao = "CIMA";
+  } else if (tecla === "ArrowDown" && direcao !== "CIMA") {
+    direcao = "BAIXO";
+  } else if (tecla === "ArrowLeft" && direcao !== "DIREITA") {
+    direcao = "ESQUERDA";
+  } else if (tecla === "ArrowRight" && direcao !== "ESQUERDA") {
+    direcao = "DIREITA";
+  }
+}
+
+function moverCobra() {
+  const cabeca = { ...cobra[0] };
+
+  switch (direcao) {
+    case "CIMA":
+      cabeca.y--;
+      break;
+    case "BAIXO":
+      cabeca.y++;
+      break;
+    case "ESQUERDA":
+      cabeca.x--;
+      break;
+    case "DIREITA":
+      cabeca.x++;
+      break;
+  }
+
+  if (verificarColisao(cabeca)) {
+    clearInterval(intervalo);
+    alert("Perdeu! :( Você fez " + pontos + " pontos.");
+    return;
+  }
+
+  cobra.unshift(cabeca);
+
+  if (cabeca.x === comida.x && cabeca.y === comida.y) {
+    pontos += 10;
+    document.getElementById("score").textContent = pontos;
+    comida = gerarComida();
+  } else {
+    cobra.pop();
+  }
+
+  criarTabuleiro();
+}
+
+function verificarColisao(cabeca) {
+  if (cabeca.x < 1 || cabeca.x > 20 || cabeca.y < 1 || cabeca.y > 20) {
+    return true;
+  }
+  for (let i = 1; i < cobra.length; i++) {
+    if (cabeca.x === cobra[i].x && cabeca.y === cobra[i].y) {
+      return true;
+    }
+  }
 }
 
 iniciar();
